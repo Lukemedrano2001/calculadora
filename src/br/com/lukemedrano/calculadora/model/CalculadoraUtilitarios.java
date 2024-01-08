9a1c30918d100696049c6a0e4c5b5fbb6f7f5af2
Lukemedrano2001 <144495981+Lukemedrano2001@users.noreply.github.com>
2024-01-08 09:47:32 -0300

    Update CalculadoraUtilitarios.java

--- src/br/com/lukemedrano/calculadora/model/CalculadoraUtilitarios.java
+++ src/br/com/lukemedrano/calculadora/model/CalculadoraUtilitarios.java
@@ -6,48 +6,46 @@ public class CalculadoraUtilitarios {
 	private CalculadoraUtilitarios() {}
 	
 	public static CalculadoraUtilitarios getInstancia() {
-        return instancia;
-    }
+        	return instancia;
+    	}
 	
 	public void resetarValores(Memoria memoria) {
-        memoria.setTextoAtual("");
-        memoria.setTextoBuffer("");
-        memoria.setSubstituir(false);
-        memoria.setUltimaOperacao(null);
-    }
+	        memoria.setTextoAtual("");
+	        memoria.setTextoBuffer("");
+	        memoria.setSubstituir(false);
+	        memoria.setUltimaOperacao(null);
+    	}
 
-    public void inverterSinal(Memoria memoria) {
-        String textoAtual = memoria.getTextoAtual();
-        if (!textoAtual.isEmpty() && !textoAtual.equals("0")) {
-            if (textoAtual.startsWith("-")) {
-                memoria.setTextoAtual(textoAtual.substring(1));
-            } else {
-                memoria.setTextoAtual("-" + textoAtual);
-            }
-        }
-    }
+    	public void inverterSinal(Memoria memoria) {
+	        String textoAtual = memoria.getTextoAtual();
+	        if (!textoAtual.isEmpty() && !textoAtual.equals("0")) {
+	            if (textoAtual.startsWith("-")) {
+	                memoria.setTextoAtual(textoAtual.substring(1));
+	            } else {
+	                memoria.setTextoAtual("-" + textoAtual);
+	            }
+	        }
+    	}
 
-    public void processarNumero(Memoria memoria, String texto) {
-        if (memoria.isSubstituir() || memoria.getTextoAtual().equals("0")) {
-            memoria.setTextoAtual(texto);
-            memoria.setSubstituir(false);
-        } else {
-            memoria.setTextoAtual(memoria.getTextoAtual() + texto);
-        }
-    }
+    	public void processarNumero(Memoria memoria, String texto) {
+	        if (memoria.isSubstituir() || memoria.getTextoAtual().equals("0")) {
+	            memoria.setTextoAtual(texto);
+	            memoria.setSubstituir(false);
+	        } else {
+	            memoria.setTextoAtual(memoria.getTextoAtual() + texto);
+	        }
+    	}
 
-    public void adicionarVirgula(Memoria memoria) {
-        String textoAtual = memoria.getTextoAtual();
-
-        if (!textoAtual.contains(",")) {
-            if (textoAtual.isEmpty() || memoria.isSubstituir()) {
-                if (!textoAtual.equals("-Infinity")) {
-                    memoria.setTextoAtual(textoAtual + "0" + ",");
-                }
-            } else {
-                memoria.setTextoAtual(textoAtual + ",");
-            }
-        }
-    }
-
+    	public void adicionarVirgula(Memoria memoria) {
+		String textoAtual = memoria.getTextoAtual();
+	        if (!textoAtual.contains(",")) {
+	            if (textoAtual.isEmpty() || memoria.isSubstituir()) {
+	                if (!textoAtual.equals("-Infinity")) {
+	                    memoria.setTextoAtual(textoAtual + "0" + ",");
+	                }
+	            } else {
+	                memoria.setTextoAtual(textoAtual + ",");
+	            }
+		}
+	}
 }
